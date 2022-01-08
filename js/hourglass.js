24029f9dc226bcb42352ab7553f0aa7741e629be
DanielSantos01 <danielspovv@gmail.com>
2022-01-08 11:00:09 -0300

    refactor: migration to class

--- js/hourglass.js
+++ js/hourglass.js
@@ -1,42 +1,50 @@
-const limit = 21;
-const isEven = limit % 2 === 0;
-let whiteSpaces = 0;
-
-console.log('#'.repeat(limit));
-
-const renderUnfilled = () => {
-  while ((whiteSpaces * 2) < (limit - 2)) {
-    const marksCount = limit - 2 - (2 * whiteSpaces);
-    console.log(`#${(' '.repeat(whiteSpaces))}${'#'.repeat(marksCount)}${' '.repeat(whiteSpaces)}#`);
-    whiteSpaces += 1;
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable class-methods-use-this */
+class Hourglass {
+  constructor() {
+    this.limit = 0;
+    this.isEven = false;
   }
 
-  whiteSpaces -= (isEven ? 1 : 2);
-
-  while (whiteSpaces >= 0) {
-    const marksCount = limit - 4 - (2 * whiteSpaces);
-    console.log(`#${(' '.repeat(whiteSpaces))}#${' '.repeat(marksCount)}#${' '.repeat(whiteSpaces)}#`);
-    whiteSpaces -= 1;
-  }
-};
-
-const renderFilled = () => {
-  while ((whiteSpaces * 2) < (limit - 3)) {
-    const marksCount = limit - 4 - (2 * whiteSpaces);
-    console.log(`#${(' '.repeat(whiteSpaces))}#${' '.repeat(marksCount)}#${' '.repeat(whiteSpaces)}#`);
-    whiteSpaces += 1;
+  setLimit(limit) {
+    this.limit = limit;
+    this.isEven = this.limit % 2 === 0;
   }
 
-  if (isEven) whiteSpaces -= 1;
+  renderFilled() {
+    let whiteSpaces = 0;
+    while ((whiteSpaces * 2) < (this.limit - 3)) {
+      const marksCount = this.limit - 4 - (2 * whiteSpaces);
+      console.log(`#${(' '.repeat(whiteSpaces))}#${' '.repeat(marksCount)}#${' '.repeat(whiteSpaces)}#`);
+      whiteSpaces += 1;
+    }
 
-  while (whiteSpaces >= 0) {
-    const marksCount = limit - 2 - (2 * whiteSpaces);
-    console.log(`#${(' '.repeat(whiteSpaces))}${'#'.repeat(marksCount)}${' '.repeat(whiteSpaces)}#`);
-    whiteSpaces -= 1;
+    if (this.isEven) whiteSpaces -= 1;
+
+    while (whiteSpaces >= 0) {
+      const marksCount = this.limit - 2 - (2 * whiteSpaces);
+      console.log(`#${(' '.repeat(whiteSpaces))}${'#'.repeat(marksCount)}${' '.repeat(whiteSpaces)}#`);
+      whiteSpaces -= 1;
+    }
   }
-};
 
-renderFilled();
+  renderUnfilled() {
+    let whiteSpaces = 0;
+    while ((whiteSpaces * 2) < (this.limit - 2)) {
+      const marksCount = this.limit - 2 - (2 * whiteSpaces);
+      console.log(`#${(' '.repeat(whiteSpaces))}${'#'.repeat(marksCount)}${' '.repeat(whiteSpaces)}#`);
+      whiteSpaces += 1;
+    }
 
-console.log('#'.repeat(limit));
+    whiteSpaces -= (this.isEven ? 1 : 2);
+
+    while (whiteSpaces >= 0) {
+      const marksCount = this.limit - 4 - (2 * whiteSpaces);
+      console.log(`#${(' '.repeat(whiteSpaces))}#${' '.repeat(marksCount)}#${' '.repeat(whiteSpaces)}#`);
+      whiteSpaces -= 1;
+    }
+  }
+}
+
+export default Hourglass;
 
