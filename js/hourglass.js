8a4f947f76d59c5f576cbde10f52dfd2913dc310
DanielSantos01 <danielspovv@gmail.com>
2022-01-08 10:52:11 -0300

    feat: filled hourglass

--- js/hourglass.js
+++ js/hourglass.js
@@ -1,18 +1,17 @@
 const limit = 21;
-const isOdd = limit % 2 === 0;
-const PREFILLED_MARKS = 2;
+const isEven = limit % 2 === 0;
 let whiteSpaces = 0;
 
 console.log('#'.repeat(limit));
 
 const renderUnfilled = () => {
   while ((whiteSpaces * 2) < (limit - 2)) {
-    const marksCount = limit - PREFILLED_MARKS - (2 * whiteSpaces);
+    const marksCount = limit - 2 - (2 * whiteSpaces);
     console.log(`#${(' '.repeat(whiteSpaces))}${'#'.repeat(marksCount)}${' '.repeat(whiteSpaces)}#`);
     whiteSpaces += 1;
   }
 
-  whiteSpaces -= (isOdd ? 1 : 2);
+  whiteSpaces -= (isEven ? 1 : 2);
 
   while (whiteSpaces >= 0) {
     const marksCount = limit - 4 - (2 * whiteSpaces);
@@ -21,7 +20,23 @@ const renderUnfilled = () => {
   }
 };
 
-renderUnfilled();
+const renderFilled = () => {
+  while ((whiteSpaces * 2) < (limit - 3)) {
+    const marksCount = limit - 4 - (2 * whiteSpaces);
+    console.log(`#${(' '.repeat(whiteSpaces))}#${' '.repeat(marksCount)}#${' '.repeat(whiteSpaces)}#`);
+    whiteSpaces += 1;
+  }
+
+  if (isEven) whiteSpaces -= 1;
+
+  while (whiteSpaces >= 0) {
+    const marksCount = limit - 2 - (2 * whiteSpaces);
+    console.log(`#${(' '.repeat(whiteSpaces))}${'#'.repeat(marksCount)}${' '.repeat(whiteSpaces)}#`);
+    whiteSpaces -= 1;
+  }
+};
+
+renderFilled();
 
 console.log('#'.repeat(limit));
 
